aa7f7fe6950abaa770e37056a06d9c4ca4e8133d
lmhseos <lmhseos@gmail.com>
2023-09-11 09:21:11 -0400

    First commit

---
+++ src/Program.cs
@@ -0,0 +1,53 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System;
+using System.Threading.Tasks;
+using DSharpPlus.EventArgs;
+using FoxBot.commands;
+using FoxBot.config;
+
+namespace FoxBot
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+
+        private static CommandsNextExtension Commands { get; set; }
+
+        public static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] {jsonReader.prefix},
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = true,
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+            Commands.RegisterCommands<GenericCommands>();
+            
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/commands/GenericCommands.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace FoxBot.commands
+{
+    public class GenericCommands : BaseCommandModule
+    {
+        [Command("Test")]
+        public async Task TestCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Hello");
+        }
+    }
+}
